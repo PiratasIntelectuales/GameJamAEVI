a8327e111a40355b2860f65ef8264f24cd990e34
SrPerso <carlos@jardiper.es>
2018-11-24 00:59:54 +0100

    UI base included
    - Start Menu
    - Stop Menu
    - Win
    - Lose

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+   public void PushQuit()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+}
