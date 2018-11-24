6c46a5ed9dc31e4d69fad281e0833481ffcd27dd
SrPerso <carlos@jardiper.es>
2018-11-24 02:15:26 +0100

    UI basic at 100% working
    next step add the shop

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -4,20 +4,84 @@ using UnityEngine;
 
 public class UiManager : MonoBehaviour {
 
+  //  public GameObject canvas;
+    Animator Uianimator;
+
+    public float Points; //puntos que tiene el jugador
+    public float LimitPoints; // limite de puntos para ganar
+
+    void Start()
+    {
+        Uianimator = GetComponent<Animator>(); 
+    }
+
     void Update()
     {
         if (Input.GetKey("escape"))
         {
-            Application.Quit();
+            GoStopMenu();
         }
+        if (LimitPoints==Points) //cuando los puntos iguala o supera pasa a la pantalla de victoria
+        {
+            Win();
+        }
+
     }
    public void PushQuit()
     {
+        Uianimator.SetBool("ToQuit", true);
         Application.Quit();
     }
 
+    public void Play()
+    {
+        Uianimator.SetBool("Playing", true);
+        Uianimator.SetBool("InStartMenu", false);
+    }
 
+    public void Resume()
+    {
+        Uianimator.SetBool("Playing", true);
+        Uianimator.SetBool("InStartMenu", false);
+        Uianimator.SetBool("InStopMenu", false);
+    }
 
+    public void Win()
+    {
+        Uianimator.SetBool("Playing", false);
+        Uianimator.SetBool("Win", true);
+        Uianimator.SetBool("Lose", false);
 
+    }
+
+    public void Lose()
+    {
+        Uianimator.SetBool("Playing", false);
+        Uianimator.SetBool("Win", false);
+        Uianimator.SetBool("Lose", true);
+
+    }
+
+    public void GoStartMenu()
+    {
+        Uianimator.SetBool("Playing", false);
+        Uianimator.SetBool("InStartMenu", true);
+        Uianimator.SetBool("InStopMenu", false);
+        Uianimator.SetBool("ToQuit", false);
+        Uianimator.SetBool("Win", false);
+        Uianimator.SetBool("Lose", false);
+
+    }
+
+    public void GoStopMenu()
+    {
+        Uianimator.SetBool("Playing", false);
+        Uianimator.SetBool("InStartMenu", false);
+        Uianimator.SetBool("InStopMenu", true);
+        Uianimator.SetBool("ToQuit", false);
+        Uianimator.SetBool("Win", false);
+        Uianimator.SetBool("Lose", false);
+
+    }
 
 }
