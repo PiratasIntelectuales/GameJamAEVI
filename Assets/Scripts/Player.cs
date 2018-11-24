b3c38655433eb12604aa6783436e737ca4b03c45
Adrian Castillo <adriancl1996@gmail.com>
2018-11-24 04:00:13 +0100

    Added seed dragging from bags, fixed bug by Eric :hamster:

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private GameObject heldObj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (heldObj == null)
+        {
+            if (Input.GetMouseButtonDown(0) == true)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    SeedBags tempScript = hit.collider.gameObject.GetComponent<SeedBags>();
+                    if (tempScript != null)
+                    {
+                        heldObj = tempScript.ClickedOn();
+                        heldObj.GetComponent<Rigidbody>().useGravity = false;
+                    }
+                }
+            }
+        }
+        else
+        {
+            float distScreen = Camera.main.WorldToScreenPoint(heldObj.transform.position).z;
+            heldObj.GetComponent<Transform>().position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distScreen));
+            if (Input.GetMouseButtonUp(0) == true)
+            {
+                heldObj.GetComponent<Rigidbody>().useGravity = true;
+                heldObj = null;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/SeedBags.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedBags : MonoBehaviour {
+    public GameObject musicSeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject ClickedOn()
+    {
+        if(gameObject.CompareTag("MusicBag"))
+        {
+            return Instantiate(musicSeed, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 0));
+        }
+
+        return null;
+    }
+}
