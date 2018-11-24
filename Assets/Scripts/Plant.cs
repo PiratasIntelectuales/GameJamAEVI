a091a4a13481e7744f66e1d8bd66f1557df75afd
Adrian Castillo <adriancl1996@gmail.com>
2018-11-24 15:31:02 +0100

    Added basic growth for plants

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+    public GameObject nextLevel;
+    public float timeToGrow = 5.0f;
+    public Vector3 offset;
+
+    private float timer = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= timeToGrow)
+        {
+            Vector3 parentPos = transform.parent.transform.position;
+            GameObject temp = Instantiate(nextLevel, transform.parent.transform);
+            temp.transform.position = new Vector3(parentPos.x + offset.x, parentPos.y + offset.y, parentPos.z + offset.z);
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pot : MonoBehaviour {
+    public GameObject musicLvl1;
+    public GameObject filmLvl1;
+
+    public Vector3 offsetmusicLvl1;
 
     private bool planted = false;
 	// Use this for initialization
@@ -17,9 +21,17 @@ public class Pot : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        if(planted == false && (other.gameObject.CompareTag("MusicSeed") == true || other.gameObject.CompareTag("FilmSeed") == true))
+        if(planted == false && other.gameObject.CompareTag("MusicSeed") == true)
         {
-            gameObject.GetComponent<Renderer>().material = other.gameObject.GetComponent<Renderer>().material;
+            GameObject temp = Instantiate(musicLvl1, transform);
+            temp.transform.position = new Vector3(transform.position.x + offsetmusicLvl1.x, transform.position.y + offsetmusicLvl1.y, transform.position.z + offsetmusicLvl1.z);
+            Destroy(other.gameObject);
+            planted = true;
+        }
+
+        else if(planted == false && other.gameObject.CompareTag("FilmSeed") == true)
+        {
+            Instantiate(filmLvl1, other.gameObject.transform);
             Destroy(other.gameObject);
             planted = true;
         }
