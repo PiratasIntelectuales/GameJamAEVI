548a3929908e69d36277c3258e9f87bf3498d6b3
HeladodePistacho <th_sola@hotmail.com>
2018-11-24 01:02:55 +0100

    Working on raycast shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -7,6 +7,9 @@ public class PlayerShoot : MonoBehaviour {
     // Use this for initialization
     public float rotation_treeshold = 0.9f;
 
+    public float time_between_shots = 0.5f;
+    public float shoot_distance = 1000;
+
 	void Start () {
 		
 	}
@@ -14,6 +17,7 @@ public class PlayerShoot : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //Rotate
         Vector2 axis = new Vector2(Input.GetAxisRaw("Secondary_Horizontal"), -Input.GetAxisRaw("Secondary_Vertical"));
 
         if (axis.magnitude > rotation_treeshold)
@@ -21,6 +25,15 @@ public class PlayerShoot : MonoBehaviour {
             float angle = (Mathf.Atan2(axis.x, axis.y) * Mathf.Rad2Deg);
             Quaternion tmp = Quaternion.AngleAxis(angle , Vector3.up);
             transform.rotation = tmp;
-        }      
+        }
+
+        //Shoot
+        Ray tmp_ray = new Ray(transform.position, transform.forward);
+        RaycastHit info;
+
+        Physics.Raycast(tmp_ray, out info, shoot_distance);
+        //info.transform;
+
+
     }
 }
